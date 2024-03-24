c7b4a7d6435b7e0359470271195aad593baa8e20
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-03-24 22:00:28 +0530

    commit

--- src/components/Caraousel.jsx
+++ src/components/Caraousel.jsx
@@ -4,7 +4,13 @@ export default function Caraousel() {
     return (
         <div>
             <div id="carouselExampleControls" className="carousel slide carousel-fade" data-bs-ride="carousel">
-                <div className="carousel-inner">
+                <div className="carousel-inner" id='carousel'>
+                    <div className='craousel-caption'>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
                     <div className="carousel-item active">
                         <img src="https://source.unsplash.com/random/500x400/?burger" className="d-block w-100" alt="..." />
                     </div>
@@ -12,7 +18,7 @@ export default function Caraousel() {
                         <img src="https://source.unsplash.com/random/900x700/?pizza" className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/900x700/?submarine" className="d-block w-100" alt="..." />
+                        <img src="https://source.unsplash.com/random/900x700/?Sandwich" className="d-block w-100" alt="..." />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -24,6 +30,6 @@ export default function Caraousel() {
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
-        </div>
+        </div >
     )
 }
