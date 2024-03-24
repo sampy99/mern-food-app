e88dee1cf63ea41f363a8708bc45a35c1523398d
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-03-24 12:39:59 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <div>
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div className="col-md-4 d-flex align-items-center">
+                    <Link to="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                        
+                    </Link>
+                    <span className="mb-3 mb-md-0 text-body-secondary">© 2024 EasyFood, Inc</span>
+                </div>
+
+                <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+                    {/* <li className="ms-3"><Link className="text-body-secondary" to="#"><svg className="bi" width="24" height="24"><use xlink:to="#twitter"></use></svg></Link></li>
+                    <li className="ms-3"><Link className="text-body-secondary" to="#"><svg className="bi" width="24" height="24"><use xlink:to="#instagram"></use></svg></Link></li>
+                    <li className="ms-3"><Link className="text-body-secondary" to="#"><svg className="bi" width="24" height="24"><use xlink:to="#facebook"></use></svg></Link></li> */}
+                </ul>
+            </footer>
+        </div>
+    )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,28 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
+import Caraousel from '../components/Caraousel'
 
 export default function Home() { 
     return(
     <div>
         <div> <Navbar/> </div>
+        <div><Caraousel/></div>
 
-    <div className="card mt-3" style={{"width": "18rem", "maxHeight":"360px"}}>
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <div className='container w-100'>
-        <select className='m-2 h-100 w-100 bg-success'>
-            {Array.from(Array(6),(e,i) => {
-                return(
-                    <option key={i+1} value={i+1}>{i+1}</option>
-                )
-            })}
-
-        </select>
-    </div>
-    </div></div>
+<div>
+    <Card/>
+</div>
 
 
         <div> <Footer/> </div>
