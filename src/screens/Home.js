2cc2786af93bcf144064fa003f82d0184d30e9d9
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-04-03 15:31:52 +0530

    commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
@@ -6,32 +6,45 @@ import Caraousel from '../components/Caraousel'
 
 export default function Home() {
 
-    const[foodCat,setfoodCat] = useState([]);
-    const[foodData,setfoodData] = useState([]);
+    const [foodCat, setfoodCat] = useState([]);
+    const [foodData, setfoodData] = useState([]);
 
     const loadData = async () => {
-        let response = await fetch("http://localhost:5000/api/foodData",{
-            method:'POST',
+        let response = await fetch("http://localhost:5000/api/foodData", {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/jason'
             }
         });
 
         response = await response.json();
-        console.log(response[0],response[1]);
+        setfoodData(response[0])
+        setfoodCat(response[1])
+        // console.log(response[0],response[1]);
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         loadData()
-    },[])
+    }, [])
 
 
     return (
         <div>
             <div> <Navbar /> </div>
             <div><Caraousel /></div>
-            <div className='m-4'><Card /><Card /><Card /><Card /></div>
-            
+            <div className='container'>
+                {
+                    foodCat !== []
+                    ? foodCat.map((data) => {
+                        return(
+                        <div>Hello World<div/>
+                        )
+                    }) 
+                    : ""
+                }
+                <Card />
+            </div>
+
             <div> <Footer /> </div>
         </div>
     )
