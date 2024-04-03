3ed168b7eff1720834ea4bad19dd39a03a9cfb9a
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-04-03 15:05:57 +0530

    commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
 import Caraousel from '../components/Caraousel'
 
 export default function Home() {
+
+    const[foodCat,setfoodCat] = useState([]);
+    const[foodData,setfoodData] = useState([]);
+
+    const loadData = async () => {
+        let response = await fetch("http://localhost:5000/api/foodData",{
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/jason'
+            }
+        });
+
+        response = await response.json();
+        console.log(response[0],response[1]);
+    }
+
+    useEffect(()=> {
+        loadData()
+    },[])
+
+
     return (
         <div>
             <div> <Navbar /> </div>
