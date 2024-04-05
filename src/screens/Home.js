4411691f687143f30b2e9544444b32f8edc3a953
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-04-06 00:22:58 +0530

    commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,6 +6,7 @@ import Caraousel from '../components/Caraousel'
 
 export default function Home() {
 
+    const [search, setSearch] = useState('');
     const [foodCat, setfoodCat] = useState([]);
     const [foodData, setfoodData] = useState([]);
 
@@ -31,37 +32,70 @@ export default function Home() {
     return (
         <div>
             <div> <Navbar /> </div>
-            <div><Caraousel /></div>
+
+{ /* Carousal is represent here directly */ }
+            <div>
+                <div id="carouselExampleControls" className="carousel slide carousel-fade" data-bs-ride="carousel" style={{ objectFit: 'contain !important' }}>
+                    <div className="carousel-inner" id='carousel'>
+                        <div className='carousel-caption' style={{ zIndex: '10' }}>
+                            <div class="d-flex justify-content-center">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e) =>{setSearch(e.target.value)}} />
+                                {/* <button class="btn btn-outline-success text-white bg-success" type="submit">Search</button> */}
+                            </div>
+                        </div>
+                        <div className="carousel-item active">
+                            <img src="https://source.unsplash.com/random/500x400/?burger" className="d-block w-100" style={{ filter: 'brightness(30%)' }} alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://source.unsplash.com/random/900x700/?pizza" className="d-block w-100" style={{ filter: 'brightness(30%)' }} alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://source.unsplash.com/random/900x700/?Sandwich" className="d-block w-100" style={{ filter: 'brightness(30%)' }} alt="..." />
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+            </div>
+
+        </div>
+
+
             <div className='container'>
                 {
                     foodCat !== []
-                    ? foodCat.map((data) => {
-                        return(<div className='row mb-3'>
-                        <div key={data._id } className='fs-3 m-3'>
-                            {data.CategoryName}
-                        </div>
-                        <hr/>
-                        {foodData !== []
-                        ? 
-                        foodData.filter((item)=> item.CategoryName ===  data.CategoryName)
-                        .map(filterItems =>{
-                            return (
-                                <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
-                                    <Card foodName = {filterItems.name}
-                                    options = {filterItems.options[0]}
-                                    imgSrc = {filterItems.img}>
-
-                                    </Card>
+                        ? foodCat.map((data) => {
+                            return (<div className='row mb-3'>
+                                <div key={data._id} className='fs-3 m-3'>
+                                    {data.CategoryName}
                                 </div>
+                                <hr />
+                                {foodData !== []
+                                    ?
+                                    foodData.filter((item) => item.CategoryName === data.CategoryName && (item.name.toLowerCase().includes(search.toLowerCase()))) 
+                                        .map(filterItems => {
+                                            return (
+                                                <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
+                                                    <Card foodName={filterItems.name}
+                                                        options={filterItems.options[0]}
+                                                        imgSrc={filterItems.img}>
+
+                                                    </Card>
+                                                </div>
+                                            )
+                                        }
+                                        ) : <div> No such data found </div>}
+                            </div>
                             )
-                        }
-                        ): <div> No such data found </div>}
-                        </div>
-                        )
-                    }) 
-                    : ""
+                        })
+                        : ""
                 }
-               
+
             </div>
 
             <div> <Footer /> </div>
