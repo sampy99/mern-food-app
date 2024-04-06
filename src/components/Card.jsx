056bf3223a9c60df889956becff097a6fc75510e
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-04-06 11:22:27 +0530

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ export default function Card(props) {
         <div>
             <div>
                 <div className="card mt-3" style={{ width: "18rem", "maxHeight": "360px" }}>
-                    <img src="https://source.unsplash.com/random/500x400/?burger" className="card-img-top" alt="..." />
+                    <img src={props.imgSrc} className="card-img-top" alt="..." style= {{height: "150px", objectFit: "fill"}} />
                     <div className="card-body">
                         <h5 className="card-title">{props.foodName}</h5>
                         <div className='container w-100'>
