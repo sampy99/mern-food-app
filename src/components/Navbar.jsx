a68756c9bb1fbfd522437dc6d415c473ad5d9895
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-04-08 15:58:22 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,11 @@ import { Link , useNavigate} from 'react-router-dom'
 import Badge from 'react-bootstrap/Badge'
 import Modal from '../Modal';
 import Cart from '../screens/Cart';
+import { useCart } from './ContextReducer';
 
 export default function Navbar() {
 
+  let data = useCart();
   const [cartView,setCartView] = useState(false);
   const navigate = useNavigate();
 const handleLogout = () => {
@@ -41,7 +43,7 @@ const handleLogout = () => {
               <div>
               <div className='btn bg-white text-success mx-1' onClick={()=>{setCartView(true)}}>
                 My Cart {" "}
-                <Badge pill bg='danger'> 2 </Badge>
+                <Badge pill bg='danger'> {data.length} </Badge>
               </div> 
 
             {cartView ? <Modal onClose={() => setCartView(false)}><Cart /></Modal> : null}
